974a241985d4bc79ac4064f0c2e3a3e0688d6257
madros <danielkoglek@gmx.at>
2021-01-31 18:13:19 +0100

    Update module.php

--- mainsail/module.php
+++ mainsail/module.php
@@ -174,7 +174,7 @@ class Mainsail extends IPSModule {
 
     private function GetThumbnail($Value) {
       $media = $this->GetIDForIdent("thumbnail");
-      IPS_SetMediaContent(29615, $Value);
+      $this->IPS_SetMediaContent(29615, $Value);
     }
 
     private function CreateThumbnail() {
