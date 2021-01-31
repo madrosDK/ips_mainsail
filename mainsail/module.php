44957c7eb3145d8da6227d59ed304c9d7b59bb15
madros <danielkoglek@gmx.at>
2021-01-31 08:41:24 +0100

    Update module.php

--- mainsail/module.php
+++ mainsail/module.php
@@ -101,7 +101,7 @@ class Mainsail extends IPSModule {
         $data = $this->RequestAPI('/printer/objects/query?virtual_sdcard');
         SetValue($this->GetIDForIdent("ProgressCompletion"), $this->FixupInvalidValue($data->result->status->virtual_sdcard->progress*100));
 
-        $data = $this->RequestAPI('/server/files/metadata?filename='GetValue(GetIDForIdent("FileName")));
+        $data = $this->RequestAPI('/server/files/metadata?filename='.GetValue(GetIDForIdent("FileName")));
         SetValue($this->GetIDForIdent("Test"), GetValue(GetIDForIdent("FileName"));
         SetValue($this->GetIDForIdent("Filament"), $this->FixupInvalidValue($data->result->filament_total));
 
