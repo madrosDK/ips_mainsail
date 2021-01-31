8ce84e562eae42775e4665bfca8d3a57dba2a694
madros <danielkoglek@gmx.at>
2021-01-31 17:24:23 +0100

    Update module.php

--- mainsail/module.php
+++ mainsail/module.php
@@ -115,7 +115,7 @@ class Mainsail extends IPSModule {
         SetValue($this->GetIDForIdent("PrintTimeLeft"), $this->CreateDuration($this->CreateUnix(GetValue($this->GetIDForIdent("TotalTime")))-$this->CreateUnix(GetValue($this->GetIDForIdent("PrintTime")))));
         SetValue($this->GetIDForIdent("PrintFinished"), $this->CreatePrintFinished($this->CreateUnix(GetValue($this->GetIDForIdent("TotalTime")))-$this->CreateUnix(GetValue($this->GetIDForIdent("PrintTime")))));
 
-        SetValue($this->GetIDForIdent("Test"), $this->FilamentETA());
+        SetValue($this->GetIDForIdent("Test"), $this->FixupInvalidValue(FilamentETA()));
         //Test zum auslesen über ID
 //        SetValue($this->GetIDForIdent("Test"), $this->CreateDuration($this->CreateUnix(GetValue($this->GetIDForIdent("TotalTime")))-$this->CreateUnix(GetValue($this->GetIDForIdent("PrintTime")))));
 //        SetValue($this->GetIDForIdent("PrintFinished"), $this->CreatePrintFinished($data->result->status->print_stats->print_duration));
