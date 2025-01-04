35bd7d057c095455ee0978d7bda2adce742b172a
madros <danielkoglek@gmx.at>
2025-01-04 12:18:16 +0100

    Update module.php

--- mainsail/module.php
+++ mainsail/module.php
@@ -133,10 +133,10 @@ class Mainsail extends IPSModule {
           SetValue($this->GetIDForIdent("ObjectHeight"), $this->FixupInvalidValue($data->result->object_height-0.4));
 
           //$data = $this->RequestAPI('/printer/info');
-          SetValue($this->GetIDForIdent("testname"), RequestAPI('/server/files/gcodes/'.->result->thumbnails[1]->relative_path));
+          //SetValue($this->GetIDForIdent("testname"), RequestAPI('/server/files/gcodes/'.->result->thumbnails[1]->relative_path));
 
           //IPS_SetMediaFile($this->GetIDForIdent("thumbnail"), $url.$data->result->thumbnails[1]->relative_path),true);
-          IPS_SetMediaContent($this->GetIDForIdent("thumbnail"), base64_encode(file_get_contents(RequestAPI('/server/files/gcodes/'.->result->thumbnails[1]->relative_path))));
+          //IPS_SetMediaContent($this->GetIDForIdent("thumbnail"), base64_encode(file_get_contents(RequestAPI('/server/files/gcodes/'.->result->thumbnails[1]->relative_path))));
         }
 
         SetValue($this->GetIDForIdent("PrintTimeLeft"), $this->CreateDuration($this->CreateUnix(GetValue($this->GetIDForIdent("TotalTime")))-$this->CreateUnix(GetValue($this->GetIDForIdent("PrintTime")))));
